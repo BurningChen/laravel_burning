3687f4a35e18c0ad72916fadc01514897a96fc99
BurningChen <BurningChen.cn@outlook.com>
2019-01-18 20:54:07 +0800

    Initial commit

---
+++ app/Http/Controllers/Show.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Show extends Controller
+{
+    public static function test()
+    {
+        echo 'test';
+    }
+
+}
